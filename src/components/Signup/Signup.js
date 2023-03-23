e39a0e26e5acdbc4a48c4414c3d25b375479e45e
mdadul <emdadulislam580@gmail.com>
2023-03-23 22:14:43 +0600

    fix error

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -105,7 +105,7 @@ export default function Signup() {
               <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
                 <input
                   type="password"
-                  id="login-password"
+                  id="confirm-password"
                   className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
                   placeholder="Confirm Password"
                   required
